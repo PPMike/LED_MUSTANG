c347e16c6a5769a021d8ca46b9efca7f7c079994
PPMike <74674465+PPMike@users.noreply.github.com>
2022-03-10 19:35:35 -0600

    Update lucesMustang.vhd

--- lucesMustang.vhd
+++ lucesMustang.vhd
@@ -1,27 +1,36 @@
+--Este codigo simula las luces direccionales de un automovil mustang
+
+--Aqui añadimos las librerias
 library IEEE;
 	use IEEE.STD_LOGIC_1164.all;
 	use IEEE.NUMERIC_STD.ALL;
-	
+
+--Aqui definimos nuestas variables
 entity maquina_de_Moore is
 	port (CLK, RST, VD, VI, IM: in STD_LOGIC;
-		LD, LI: out STD_LOGIC_VECTOR(3 downto 0));
+		LD, LI: out STD_LOGIC_VECTOR(3 downto 0)); --Estas variables son un vector de 3 a 0 que representan cada led
 end maquina_de_Moore;
 
+--Aqui definimos todos los estados posibles que podemos tener
 architecture A1 of maquina_de_Moore is
 
 type State is (S0, SIZ0, SIZ1, SIZ2, SIZ3, SID0, SID1, SID2, SID3, SIN0, SIN1);
-
+--Aqui se definen los cambios de estado
 signal CurrentState, NextState: State;
 signal count: integer:=1;
 signal temp: std_logic:='0';
 
 begin
+	--Este process sera el encargado de hacer los cambios de estado
 	Estados: process(CLK, RST)
 	begin
+		--Cuando hacemos reset nuestro primer estado se manda al estado que estamos corriendo
 		if (RST = '1') then
 			CurrentState <= S0;
+		--Cuando hay un cambio de reloj aumentamos el contados y cambiamos al siguiente estado programado
 		elsif(rising_edge(CLK)) then
 			count <= count + 1;
+			--Aqui reiniciamos el ciclo de reloj cada segundo
 			if (count = 25000000) then
 				count <= 1;
 				CurrentState <= NextState;
@@ -29,6 +38,7 @@ begin
 		end if;
 	end process Estados;
 	
+	--Esto es el process de las lucese
 	Luces: process(CurrentState, RST, IM, VD, VI)
 	begin 
 		NextState <= CurrentState;
